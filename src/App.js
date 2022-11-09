8a27ebff4b83a68c93519f8ceaa7e9f19ddab29b
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-11-09 22:35:09 +0800

    app.js setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu'
+import './styles/global.css'
+import HeadWave from './components/HeadWave'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container">
+        <div className="row top-section">
+          <HeadWave />
+          <Menu />
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
