2e2b40ee48c27cc8b92f8a1a4bd4420b2a0e725a
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-11-09 22:35:18 +0800

    add Menu

---
+++ src/components/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Menu.css'
+
+function Menu() {
+  return (
+    <>
+      <div className="btn-border">
+        <h2>MENU</h2>
+        <div className="menu-icon">
+          <i class="fa-solid fa-bars"></i>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Menu
