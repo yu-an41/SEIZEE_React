2699045a6b679c95668a660bbad237e106f33371
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-11-13 16:44:56 +0800

    add <YellowWave />

---
+++ src/00-homepage/components/YellowWave.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './../styles/YellowWave.scss'
+
+function YellowWave() {
+  return (
+    <div className="y-wave-section">
+      <div className="y-wave-base"></div>
+      <div className="y-wave-wrap"></div>
+    </div>
+  )
+}
+
+export default YellowWave
--- src/00-homepage/pages/Index.js
+++ src/00-homepage/pages/Index.js
@@ -1,22 +1,17 @@
 import React from 'react'
 import './../styles/Index.scss'
+import YellowWave from '../components/YellowWave'
 
 function Index() {
   return (
     <>
-      <div>
-        Index
-        <div className="y-index-container">
-          <section className="y-section-nav">
-            <div className="y-wave-section">
-              <div class="wave-base"></div>
-              <div class="wave"></div>
-            </div>
-            <p className="y-test">loremmmmm</p>
-          </section>
-          <section className="y-section-carousel">carousel</section>
-          <section className="y-section-nav">nav</section>
-        </div>
+      <div className="y-index-container">
+        <section className="y-section-nav">
+          nav
+          <YellowWave />
+        </section>
+        <section className="y-section-carousel">carousel</section>
+        <section className="y-section-nav">nav</section>
       </div>
     </>
   )
