9c6a3575d8f562b4ad5cc7ca5747cdbfd385aa26
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-11-18 10:55:17 +0800

    Merge remote-tracking branch 'origin/sharon'

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ import Homepage from './00-homepage/pages/Homepage'
 import Menu from './components/Menu'
 import CartList from './01-cart/pages/CartList'
 
+// 05-member
+import SignUp from './05-member/SignUp'
+import ResetPass from './05-member/ResetPass'
+import ForgotPass from './05-member/ForgotPass'
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/cart" element={<CartList />} />
-      </Routes>
+      <MyContextProviders>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/cart" element={<CartList />} />
+
+          {/* 05-member */}
+          <Route path="/login" element={<SignUp />} />
+          <Route path="/forgot-pass" element={<ForgotPass />} />
+          <Route path="/reset-pass" element={<ResetPass />} />
+          {/* <div className="container">
+              <div className="row top-section">
+                <Route path="/login" element={<SignUp />} />
+              </div>
+            </div> */}
+        </Routes>
+      </MyContextProviders>
     </BrowserRouter>
   )
 }
