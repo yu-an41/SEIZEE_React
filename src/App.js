d4bf3ac978280075df3c3de5811d4709a09a4681
James Tsai <acidman1127@gmail.com>
2022-11-18 12:26:59 +0800

    first push dont kill me

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Menu from './components/Menu'
-import MyContextProviders from './contexts/MyContextProviders';
+import MyContextProviders from './contexts/MyContextProviders'
 import './styles/global.css'
 import HeadWave from './components/HeadWave'
-
+import Top from './06-event/pages/06-event-01-top'
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <MyContextProviders>
-        <Routes>
-          <div className="container">
-            <div className="row top-section">
+      <BrowserRouter>
+        {/* <MyContextProviders> */}
+        {/* <Routes> */}
+        {/* <div className="container"> */}
+        {/* <div className="row top-section">
             </div>
-          </div>
-          </Routes>
-        </MyContextProviders>
+          </div> */}
+        {/* </Routes> */}
+        {/* </MyContextProviders> */}
+        <Top/>
       </BrowserRouter>
     </>
   )
