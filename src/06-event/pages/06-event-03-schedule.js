f2a42ea0da0fe46107ff913e43b9127442842915
James Tsai <acidman1127@gmail.com>
2022-11-19 14:36:14 +0800

    03 schedule js+scss added

--- src/06-event/pages/06-event-03-schedule.js
+++ src/06-event/pages/06-event-03-schedule.js
@@ -178,263 +178,263 @@ function Schedule() {
               <div class="j-list-dash-8"></div>
               <div class="j-list-dash-9"></div>
               <div class="j-cookA">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <div class="schedule-card-time">12:00-13:00</div>
-                    <div class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <div class="j-schedule-card-time">12:00-13:00</div>
+                    <div class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </div>
                   </div>
-                  <div class="schedule-card-name">
+                  <div class="j-schedule-card-name">
                     全食物利用 <br />
                     -水果酵釀-
                   </div>
-                  <div class="schedule-card-hashtag">#穢土轉生#工作坊</div>
+                  <div class="j-schedule-card-hashtag">#穢土轉生#工作坊</div>
                 </div>
               </div>
               <div class="j-cookB">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <div class="schedule-card-time">14:00-15:00</div>
-                    <div class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <div class="j-schedule-card-time">14:00-15:00</div>
+                    <div class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </div>
                   </div>
-                  <div class="schedule-card-name">
+                  <div class="j-schedule-card-name">
                     全食物利用 <br />
                     -水果酵釀-
                   </div>
-                  <div class="schedule-card-hashtag">#穢土轉生#工作坊</div>
+                  <div class="j-schedule-card-hashtag">#穢土轉生#工作坊</div>
                 </div>
               </div>
               <div class="j-cookC">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <div class="schedule-card-time">16:00-17:00</div>
-                    <div class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <div class="j-schedule-card-time">16:00-17:00</div>
+                    <div class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </div>
                   </div>
-                  <div class="schedule-card-name">
+                  <div class="j-schedule-card-name">
                     全食物利用 <br />
                     -柚酵釀-
                   </div>
-                  <div class="schedule-card-hashtag">#穢土轉生#工作坊</div>
+                  <div class="j-schedule-card-hashtag">#穢土轉生#工作坊</div>
                 </div>
               </div>
               <div class="j-cookD">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <div class="schedule-card-time">18:00-19:00</div>
-                    <div class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <div class="j-schedule-card-time">18:00-19:00</div>
+                    <div class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </div>
                   </div>
-                  <div class="schedule-card-name">
+                  <div class="j-schedule-card-name">
                     全食物利用 <br />
                     -特製莎莎醬-
                   </div>
-                  <div class="schedule-card-hashtag">#穢土轉生#工作坊</div>
+                  <div class="j-schedule-card-hashtag">#穢土轉生#工作坊</div>
                 </div>
               </div>
               <div class="j-musicA">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <div class="schedule-card-time">13:00-14:00</div>
-                    <div class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <div class="j-schedule-card-time">13:00-14:00</div>
+                    <div class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </div>
                   </div>
-                  <div class="schedule-card-name">泥灘地浪人</div>
-                  <div class="schedule-card-hashtag">#橘色舞台#音樂演出</div>
+                  <div class="j-schedule-card-name">泥灘地浪人</div>
+                  <div class="j-schedule-card-hashtag">#橘色舞台#音樂演出</div>
                 </div>
               </div>
               <div class="j-musicB">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <div class="schedule-card-time">15:00-16:00</div>
-                    <div class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <div class="j-schedule-card-time">15:00-16:00</div>
+                    <div class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </div>
                   </div>
-                  <div class="schedule-card-name">顏社煮場秀</div>
-                  <div class="schedule-card-hashtag">#橘色舞台#音樂演出</div>
+                  <div class="j-schedule-card-name">顏社煮場秀</div>
+                  <div class="j-schedule-card-hashtag">#橘色舞台#音樂演出</div>
                 </div>
               </div>
               <div class="j-musicC">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <div class="schedule-card-time">17:00-18:00</div>
-                    <div class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <div class="j-schedule-card-time">17:00-18:00</div>
+                    <div class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </div>
                   </div>
-                  <div class="schedule-card-name">Robot Swing</div>
-                  <div class="schedule-card-hashtag">#橘色舞台#音樂演出</div>
+                  <div class="j-schedule-card-name">Robot Swing</div>
+                  <div class="j-schedule-card-hashtag">#橘色舞台#音樂演出</div>
                 </div>
               </div>
               <div class="j-musicD">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <div class="schedule-card-time">19:00-20:00</div>
-                    <div class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <div class="j-schedule-card-time">19:00-20:00</div>
+                    <div class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </div>
                   </div>
-                  <div class="schedule-card-name">Toe</div>
-                  <div class="schedule-card-hashtag">#橘色舞台#音樂演出</div>
+                  <div class="j-schedule-card-name">Toe</div>
+                  <div class="j-schedule-card-hashtag">#橘色舞台#音樂演出</div>
                 </div>
               </div>
               <div class="j-tedA">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <span class="schedule-card-time">12:00-13:00</span>
-                    <span class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <span class="j-schedule-card-time">12:00-13:00</span>
+                    <span class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </span>
                   </div>
-                  <div class="schedule-card-name">Means Database</div>
-                  <span class="schedule-card-hashtag">#泰迪托克#講座</span>
+                  <div class="j-schedule-card-name">Means Database</div>
+                  <span class="j-schedule-card-hashtag">#泰迪托克#講座</span>
                 </div>
               </div>
               <div class="j-tedB">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <span class="schedule-card-time">14:00-15:00</span>
-                    <span class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <span class="j-schedule-card-time">14:00-15:00</span>
+                    <span class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </span>
                   </div>
-                  <div class="schedule-card-name">Miss Eco</div>
-                  <span class="schedule-card-hashtag">#泰迪托克#講座</span>
+                  <div class="j-schedule-card-name">Miss Eco</div>
+                  <span class="j-schedule-card-hashtag">#泰迪托克#講座</span>
                 </div>
               </div>
               <div class="j-tedC">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <span class="schedule-card-time">16:00-17:00</span>
-                    <span class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <span class="j-schedule-card-time">16:00-17:00</span>
+                    <span class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </span>
                   </div>
-                  <div class="schedule-card-name">Taste Me</div>
-                  <span class="schedule-card-hashtag">#泰迪托克#講座</span>
+                  <div class="j-schedule-card-name">Taste Me</div>
+                  <span class="j-schedule-card-hashtag">#泰迪托克#講座</span>
                 </div>
               </div>
               <div class="j-tedD">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <span class="schedule-card-time">18:00-19:00</span>
-                    <span class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <span class="j-schedule-card-time">18:00-19:00</span>
+                    <span class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </span>
                   </div>
-                  <div class="schedule-card-name">SEIZEE</div>
-                  <span class="schedule-card-hashtag">#泰迪托克#講座</span>
+                  <div class="j-schedule-card-name">SEIZEE</div>
+                  <span class="j-schedule-card-hashtag">#泰迪托克#講座</span>
                 </div>
               </div>
               <div class="j-cyberA">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <span class="schedule-card-time">13:00-14:00</span>
-                    <span class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <span class="j-schedule-card-time">13:00-14:00</span>
+                    <span class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </span>
                   </div>
-                  <div class="schedule-card-name">2022廚房漫遊</div>
-                  <span class="schedule-card-hashtag">#合法幻舞#VR體驗</span>
+                  <div class="j-schedule-card-name">2022廚房漫遊</div>
+                  <span class="j-schedule-card-hashtag">#合法幻舞#VR體驗</span>
                 </div>
               </div>
               <div class="j-cyberB">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <span class="schedule-card-time">15:00-16:00</span>
-                    <span class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <span class="j-schedule-card-time">15:00-16:00</span>
+                    <span class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </span>
                   </div>
-                  <div class="schedule-card-name">惜食超能100</div>
-                  <span class="schedule-card-hashtag">#合法幻舞#VR體驗</span>
+                  <div class="j-schedule-card-name">惜食超能100</div>
+                  <span class="j-schedule-card-hashtag">#合法幻舞#VR體驗</span>
                 </div>
               </div>
               <div class="j-cyberC">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <span class="schedule-card-time">17:00-18:00</span>
-                    <span class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <span class="j-schedule-card-time">17:00-18:00</span>
+                    <span class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </span>
                   </div>
-                  <div class="schedule-card-name">快對醜蔬果出手</div>
-                  <span class="schedule-card-hashtag">#合法幻舞#VR體驗</span>
+                  <div class="j-schedule-card-name">快對醜蔬果出手</div>
+                  <span class="j-schedule-card-hashtag">#合法幻舞#VR體驗</span>
                 </div>
               </div>
               <div class="j-cyberD">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <span class="schedule-card-time">19:00-20:00</span>
-                    <span class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <span class="j-schedule-card-time">19:00-20:00</span>
+                    <span class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </span>
                   </div>
-                  <div class="schedule-card-name">搶救萎吉大兵</div>
-                  <span class="schedule-card-hashtag">#合法幻舞#VR體驗</span>
+                  <div class="j-schedule-card-name">搶救萎吉大兵</div>
+                  <span class="j-schedule-card-hashtag">#合法幻舞#VR體驗</span>
                 </div>
               </div>
               <div class="j-showA">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <span class="schedule-card-time">12:00-13:00</span>
-                    <span class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <span class="j-schedule-card-time">12:00-13:00</span>
+                    <span class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </span>
                   </div>
-                  <div class="schedule-card-name">
+                  <div class="j-schedule-card-name">
                     令人討厭的
                     <br />
                     醜食的一生
                   </div>
-                  <span class="schedule-card-hashtag">#合法幻舞#VR體驗</span>
+                  <span class="j-schedule-card-hashtag">#合法幻舞#VR體驗</span>
                 </div>
               </div>
               <div class="j-showB">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <span class="schedule-card-time">14:00-15:00</span>
-                    <span class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <span class="j-schedule-card-time">14:00-15:00</span>
+                    <span class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </span>
                   </div>
-                  <div class="schedule-card-name">
+                  <div class="j-schedule-card-name">
                     歡迎來到好浪費
                     <br />
                     現代社會
                   </div>
-                  <span class="schedule-card-hashtag">#合法幻舞#VR體驗</span>
+                  <span class="j-schedule-card-hashtag">#合法幻舞#VR體驗</span>
                 </div>
               </div>
               <div class="j-showC">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <span class="schedule-card-time">16:00-17:00</span>
-                    <span class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <span class="j-schedule-card-time">16:00-17:00</span>
+                    <span class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </span>
                   </div>
-                  <div class="schedule-card-name">醜蔬果特派週報</div>
-                  <span class="schedule-card-hashtag">#合法幻舞#VR體驗</span>
+                  <div class="j-schedule-card-name">醜蔬果特派週報</div>
+                  <span class="j-schedule-card-hashtag">#合法幻舞#VR體驗</span>
                 </div>
               </div>
               <div class="j-showD">
-                <div class="schedule-card">
-                  <div class="schedule-card-head">
-                    <span class="schedule-card-time">18:00-19:00</span>
-                    <span class="schedule-card-heart">
+                <div class="j-schedule-card">
+                  <div class="j-schedule-card-head">
+                    <span class="j-schedule-card-time">18:00-19:00</span>
+                    <span class="j-schedule-card-heart">
                       <img src={jHeart} alt="" />
                     </span>
                   </div>
-                  <div class="schedule-card-name">高年級惜食生</div>
-                  <span class="schedule-card-hashtag">#合法幻舞#VR體驗</span>
+                  <div class="j-schedule-card-name">高年級惜食生</div>
+                  <span class="j-schedule-card-hashtag">#合法幻舞#VR體驗</span>
                 </div>
               </div>
             </div>
