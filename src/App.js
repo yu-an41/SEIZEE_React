9bd0e9cea80f67b71fe3e2023ea03ae17e5fe820
James Tsai <acidman1127@gmail.com>
2022-11-21 14:11:48 +0800

    11/21-14:11

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import MyContextProviders from './contexts/MyContextProviders'
 import './styles/global.css'
 import HeadWave from './components/HeadWave'
 import Top from './06-event/pages/06-event-01-top'
-<<<<<<< HEAD
+
 import Events from './06-event/pages/06-event-02-events'
 import Schedule from './06-event/pages/06-event-03-schedule'
 import Map from './06-event/pages/06-event-04-map'
 import Ticket from './06-event/pages/06-event-05-ticket'
 
-
-=======
->>>>>>> d4bf3ac978280075df3c3de5811d4709a09a4681
 function App() {
   return (
     <>
@@ -25,17 +22,11 @@ function App() {
           </div> */}
         {/* </Routes> */}
         {/* </MyContextProviders> */}
-<<<<<<< HEAD
         {/* <Top/> */}
-        <Events/>
+        <Events />
         {/* <Schedule /> */}
         {/* <Map /> */}
         {/* <Ticket /> */}
-        
-
-=======
-        <Top/>
->>>>>>> d4bf3ac978280075df3c3de5811d4709a09a4681
       </BrowserRouter>
     </>
   )
