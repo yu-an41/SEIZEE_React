d59e0fac28c3850b500229bfe7610540ecd2580a
Sharon Yu <yu5286pp@gmail.com>
2022-11-21 13:05:05 +0800

    updateinfo city selection

--- src/05-member/profile-pages/UpdateInfo copy.js
+++ src/05-member/profile-pages/UpdateInfo copy.js
@@ -2,70 +2,11 @@ import '.././style/profile-pages/UpdateInfo.scss'
 
 import React, { useEffect, useRef, useState } from 'react'
 import UserProfileTmp from '../components/UserProfileTmp'
-import { DistrictData } from '../data/address'
-import { map, find, propEq, forEach, isNil } from 'ramda'
-import Select from 'react-select'
-import axios from 'axios'
-import { UPDATE_PASS } from '../../my-config'
-
-// selectedCity
-const selectedCity = (cityName) => ({ value: cityName, label: cityName })
-// selectedDistrict
-const selectedDistrict = (districtName) => ({
-  value: districtName,
-  label: districtName,
-})
-// 當前縣市選項
-const cities = () =>
-  map((city) => ({ value: city.name, label: city.name }), DistrictData)
-// 當前區域選項
-const findDistricts = (cityName) =>
-  find(propEq('name', cityName))(DistrictData)?.districts
-// 區域選項 (符合下拉式選單的格式)
-const districtOpts = (cityName) =>
-  map(
-    (d) => ({ value: d.name, label: d.name, zip: d.zip }),
-    findDistricts(cityName)
-  )
-// 使用區域尋找，呼叫方式: queryByDistrict('新莊區')
-const queryByDistrict = (district) => queryProcess('value', district)
-// 使用區域代碼尋找、使用區域尋找共用元件
-const queryProcess = (type, params) => {
-  // 處理 params 空值的狀況
-  if (isNil(params)) return
-  // 所有的縣市名
-  const citiesName = map((city) => city.name, DistrictData)
-  let foundedObj = null
-  // 從單一縣市內找尋的動作
-  const findAction = (cityName) => {
-    // 依據區域代碼找尋縣市內的地區資訊
-    const found = find(propEq(type, params))(districtOpts(cityName))
-    // 找不到回傳空值
-    if (isNil(found)) return
-    // 找到則將找到的值指派給foundedObj
-    // e.g. => { value: "新莊區", label: "新莊區", zip: "242", city: "新北市" }
-    foundedObj = { ...found, city: cityName }
-  }
-  // 執行動作
-  forEach(findAction, citiesName)
-  // 回傳找到的物件
-  return foundedObj
-}
 
 function UpdateInfo(props) {
   // -----讀取地址-----
-  // 被選區域
-  const [district, setDistrict] = useState(props?.district)
-  // 被選縣市
-  const [city, setCity] = useState(queryByDistrict(district)?.city)
-  // 被選縣市的相依區域
-  const [districts, setDistricts] = useState(null)
-  // city 改變對應動作
-  useEffect(() => {
-    if (!isNil(city)) setDistricts(districtOpts(city))
-  }, [city])
-
-  console.log(district)
+  const [selectCity, setSelectCity] = useState('')
+  const [selectArea, setSelectArea] = useState('')
 
   // -----檔案上傳-----
   // 選擇的檔案
@@ -79,24 +20,9 @@ function UpdateInfo(props) {
   // Trigger the clicking of the input element
   const hiddenFileInput = useRef(null)
 
-  // -----更新會員資料-----
-  // 更新會員資料
-  const [updateFD, setUpdateFD] = useState({
-    mbrEmail: '',
-    mbrName: '',
-    mbrPass: '',
-    mbrPassConfirm: '',
-  })
   // =================================================
 
   // -----讀取地址-----
-  // city change 事件處理
-  const handleCityChange = (e) => {
-    setDistrict('')
-    setCity(e.value)
-
-    console.log(e.value)
-  }
 
   // -----檔案上傳-----
   // 使用點擊image去呼叫input file 才可以開啟資料夾選擇照片
@@ -135,12 +61,7 @@ function UpdateInfo(props) {
     }
   }
 
-  // ====================================
-  // 註冊
-  const updateSubmit = async (e) => {
-    e.preventDefault()
-    const { data } = await axios.post(UPDATE_PASS, updateFD)
-  }
+  const updateSubmit = () => {}
 
   return (
     <>
@@ -177,23 +98,21 @@ function UpdateInfo(props) {
                       <div className="s-ui-answer">yu5286pp@gmail.com</div>
                     </div>
                   </div>
-
                   <label className="s-ui-label">性別</label>
+
                   <div className="s-ui-radio1">
                     <input
                       type="radio"
-                      name="mbuGender"
+                      name="fav_color"
                       value="男"
-                      id="mbuBoy"
                       className="s-ui-radio2"
                     />
                     <span className="s-ui-man">男</span>
                     <br />
                     <input
                       type="radio"
-                      name="mbuGender"
+                      name="fav_color"
                       value="女"
-                      id="mbuGirl"
                       className="s-ui-radio2"
                     />
                     <span className="s-ui-man">女</span>
@@ -201,32 +120,16 @@ function UpdateInfo(props) {
 
                   <label className="s-ui-label">預設地址</label>
                   <div className="s-ui-selectAddress">
-                    <Select
-                      value={city ? selectedCity(city) : ''}
-                      id="selectCity"
-                      className="s-ui-address"
-                      name={props?.cityName}
-                      options={cities()}
-                      // value={selectedCity(city)}
-                      onChange={handleCityChange}
-                      placeholder="選擇城市"
-                    />
-                    {/* <option value="" className="s-ui-addressDetail">
+                    <select name="" id="selectCity" className="s-ui-address">
+                      <option value="" className="s-ui-addressDetail">
                         請選擇城市
-                      </option> */}
-                    <Select
-                      value={district ? selectedDistrict(district) : ''}
-                      id="selectArea"
-                      className="s-ui-address"
-                      name={props?.districtName}
-                      // value={selectedDistrict(district)}
-                      options={districts}
-                      placeholder="選擇區域"
-                      onChange={(e) => setDistrict(e.value)}
-                    />
-                    {/* <option value="" className="s-ui-addressDetail">
-                      請選擇地區
-                    </option> */}
+                      </option>
+                    </select>
+                    <select name="" id="selectArea" className="s-ui-address">
+                      <option value="" className="s-ui-addressDetail">
+                        請選擇地區
+                      </option>
+                    </select>
                   </div>
                   <input
                     className="s-ui-input"
@@ -257,6 +160,8 @@ function UpdateInfo(props) {
   )
 }
 
-// reference: https://medium.com/web-dev-survey-from-kyoto/how-to-customize-the-file-upload-button-in-react-b3866a5973d8
+// reference:
+// 1. https://medium.com/web-dev-survey-from-kyoto/how-to-customize-the-file-upload-button-in-react-b3866a5973d8
+// 2. https://chenhanting.medium.com/react-rails-%E5%8F%B0%E7%81%A3%E5%8D%80%E7%B8%A3%E5%B8%82-%E5%8D%80%E5%9F%9F%E7%9A%84%E4%B8%8B%E6%8B%89%E9%81%B8%E5%96%AE-ffe75285a443
 
 export default UpdateInfo
