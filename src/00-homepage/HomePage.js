8c7c60ad20d9593ef5e574f4af6a768bf3abb340
Sharon Yu <yu5286pp@gmail.com>
2022-11-24 15:26:32 +0800

    add images for navbar

--- src/00-homepage/HomePage.js
+++ src/00-homepage/HomePage.js
@@ -1,13 +1,15 @@
 import Runman from '../components/Runman'
 import ModalConfirm from '../components/ModalConfirm'
 import ModalNotification from '../components/ModalNotification'
+// import NavBar from '/components/NavBar'
 
 function HomePage() {
   return (
     <>
       {/* <Runman /> */}
       {/* <ModalConfirm /> */}
-      <ModalNotification />
+      {/* <ModalNotification /> */}
+      {/* <NavBar /> */}
     </>
   )
 }
