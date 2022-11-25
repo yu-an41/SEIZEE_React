5b1c62f86d369c2f4c02c60442b14ead6a627cc6
Ariel <ariel50515@gmail.com>
2022-11-25 16:30:32 +0800

    輪播牆

--- src/04-product/components/Carousel.js
+++ src/04-product/components/Carousel.js
@@ -1,55 +1,67 @@
-import React, { useState } from 'react'
-import './style/Carousel.scss'
+import React, { useState } from "react";
+import "./style/Carousel.scss";
 
 export default function Carousel() {
-  const [myArray, setMyArray] = useState(['0', '1', '2'])
+  const [myArray, setMyArray] = useState(["0", "1", "2"]);
 
   function handleClick(e) {
-    console.log('e', e.target.innerText)
+    switch (e) {
+      case 0:
+        setMyArray(["1", "2", "0"]);
+        break;
+      case 1:
+        setMyArray(["0", "1", "2"]);
+        break;
+      case 2:
+        setMyArray(["2", "0", "1"]);
+        break;
+      default:
+    }
+    return;
+    console.log("e", e.currentTarget.innerText);
 
-    if (e.target.innerText === '0') {
-      setMyArray(['1', '2', '0'])
+    if (e.currentTarget.innerText[0] === "0") {
+      setMyArray(["1", "2", "0"]);
     }
-    if (e.target.innerText === '1') {
-      setMyArray(['0', '1', '2'])
+    if (e.currentTarget.innerText[0] === "1") {
+      setMyArray(["0", "1", "2"]);
     }
-    if (e.target.innerText === '2') {
-      setMyArray(['2', '0', '1'])
+    if (e.currentTarget.innerText[0] === "2") {
+      setMyArray(["2", "0", "1"]);
     }
   }
 
   return (
-    <div style={{ display: 'flex' }}>
+    <div className="a-caroudelWrapper">
       <div
-        className="bg-1"
+        className={myArray[0] == 1 ? "myselectedImage" : ""}
         style={{
           order: myArray[0],
         }}
         onClick={(e) => {
-          handleClick(e)
+          handleClick(0);
         }}
       >
-      <img src='/04-product/img/10003' alt='' />
+        <img src="/04-product/img/10003.jpg" alt="" />
       </div>
       <div
-        className="bg-2"
+        className={myArray[1] == 1 ? "myselectedImage" : ""}
         style={{
           order: myArray[1],
         }}
-        onClick={(e) => handleClick(e)}
+        onClick={(e) => handleClick(1)}
       >
-      <img src='/04-product/img/10013' alt='' />
+        <img src="/04-product/img/10013.jpg" alt="" />
       </div>
       <div
-        className="bg-3"
+        className={myArray[2] == 1 ? "myselectedImage" : ""}
         style={{
           order: myArray[2],
         }}
-        onClick={(e) => handleClick(e)}
+        onClick={(e) => handleClick(2)}
       >
-      <img src='/04-product/img/10006' alt='' />
+        <img src="/04-product/img/10006.jpg" alt="" />
       </div>
     </div>
-  )
+  );
 }
-
