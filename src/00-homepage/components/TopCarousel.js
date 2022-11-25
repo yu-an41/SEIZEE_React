3e32125379c0c15f0458dee06268283bffb2cfe8
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-11-25 10:04:51 +0800

    edit TopCarousel

--- src/00-homepage/components/TopCarousel.js
+++ src/00-homepage/components/TopCarousel.js
@@ -3,10 +3,6 @@ import Slider from 'react-slick'
 
 import './../styles/TopCarousel.scss'
 
-import BannerImage1 from './../../dotown/sushi-with-egg.png'
-import BannerImage2 from './../../dotown/pizza.png'
-import BannerImage3 from './../../dotown/toast.png'
-
 export default class TopCarousel extends Component {
   render() {
     var settings = {
@@ -23,16 +19,22 @@ export default class TopCarousel extends Component {
       <div className="y-top-carousel-container">
         <Slider {...settings}>
           <div className="y-top-carousel-banner">
-            <p className="y-h3">1</p>
-            <img src={BannerImage1} alt="banner carousel" />
+            <img
+              src="/00-homepage/banner1-ice-cream.png"
+              alt="banner carousel"
+            />
           </div>
           <div className="y-top-carousel-banner">
-            <p className="y-h3">1</p>
-            <img src={BannerImage2} alt="banner carousel" />
+            <img
+              src="/00-homepage/banner1-ice-cream.png"
+              alt="banner carousel"
+            />
           </div>
           <div className="y-top-carousel-banner">
-            <p className="y-h3">1</p>
-            <img src={BannerImage3} alt="banner carousel" />
+            <img
+              src="/00-homepage/banner1-ice-cream.png"
+              alt="banner carousel"
+            />
           </div>
         </Slider>
       </div>
