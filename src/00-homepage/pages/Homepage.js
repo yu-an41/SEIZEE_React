43d9b907494970e26468c860ffeb0e1703a5ef9f
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-11-27 11:46:31 +0800

    add AboutUs & NewEvents

--- src/00-homepage/pages/Homepage.js
+++ src/00-homepage/pages/Homepage.js
@@ -1,10 +1,12 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect, useCallback, useRef } from 'react'
 import axios from 'axios'
 import { toppings } from './../../03-shop/toppings'
 
 // scss
 import './../styles/Homepage.scss'
 import './../../03-shop/styles/03-shop-home.scss'
+// import './../styles/AboutUs.scss'
+// import './../styles/NewEvent.scss'
 
 // components
 import NavBar from '../components/NavBar'
@@ -76,6 +78,20 @@ function Homepage() {
   }, [])
 
   // console.log(shops);
+
+  const videoEl = useRef(null)
+
+  const attemptPlay = () => {
+    videoEl &&
+      videoEl.current &&
+      videoEl.current.play().catch((error) => {
+        console.error('Error attempting to play', error)
+      })
+  }
+
+  useEffect(() => {
+    attemptPlay()
+  }, [])
 
   return (
     <>
@@ -246,8 +262,77 @@ function Homepage() {
         <div className="y-wave-wrap">
           <YellowWave />
         </div>
-        <section className="y-section y-section-about">about</section>
-        <section className="y-section y-section-event">event</section>
+        <section className="y-section y-section-about">
+          <div className="a-video">
+            <div className="a-videoWrapper">
+              <h3 className="a-aboutUs">關於我們</h3>
+              <video
+                playsInline
+                loop
+                muted
+                alt="All the devices"
+                src="../../video/test.mp4"
+                type="video/mp4"
+                ref={videoEl}
+              />
+            </div>
+          </div>
+        </section>
+        <section className="y-section y-section-event">
+          <div class="a-eventBanner">
+            <div className="a-eventWrapper">
+              <h3 className="a-newEvent">最新活動</h3>
+              <div class="a-eventText">
+                <h1 className="a-whatFun">What's New Fun</h1>
+              </div>
+              <div className="a-eventContent">
+                <div className="a-calendarWrapper">
+                  <div class="a-imgWrapper">
+                    <img src="./svg/calendar.svg" alt="" />
+                  </div>
+                  <div class="a-dateWrapper">
+                    <h2 className="a-month">Dec.</h2>
+                    <h2 className="a-day">25</h2>
+                  </div>
+                </div>
+                <div class="a-eventContentWrapper">
+                  <div className="a-event">
+                    <div className="a-eventDayWrapper">
+                      <h4>Dec.</h4>
+                      <h4>25</h4>
+                    </div>
+                    <div class="a-eventImgWrapper">
+                      <img src="../../04-product/event.svg" alt="" />
+                    </div>
+                    <div className="a-eventName">
+                      <h4>劇場</h4>
+                      <h4>快對醜蔬果出手</h4>
+                    </div>
+                    <div className="a-eventButton">
+                      <h4>詳細</h4>
+                    </div>
+                  </div>
+                  <div className="a-event">
+                    <div className="a-eventDayWrapper">
+                      <h4>Dec.</h4>
+                      <h4>25</h4>
+                    </div>
+                    <div class="a-eventImgWrapper">
+                      <img src="../../04-product/calendar.svg" alt="" />
+                    </div>
+                    <div className="a-eventName">
+                      <h4>劇場</h4>
+                      <h4>快對醜蔬果出手</h4>
+                    </div>
+                    <div className="a-eventButton">
+                      <h4>詳細</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
         <div className="y-wave-wrap">
           <YellowWave />
         </div>
