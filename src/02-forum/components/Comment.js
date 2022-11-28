b7fb6b91b0f87bc49fd40a30d3181370a37ae104
Shen Pei <shen.pei.wun@gmail.com>
2022-11-28 11:52:02 +0800

    留言、頻道切換

---
+++ src/02-forum/components/Comment.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+
+import '../styles/Message.scss'
+
+import Member from './Member'
+function Comment({ commData }) {
+  const { content, created_at } = commData
+
+  return (
+    <>
+      <div className="p-message">
+        <div className="p-messMembAdCreat">
+          <div className="p-messageMember">
+            <Member />
+          </div>
+          <p className="p-messCreatAt">{created_at}</p>
+        </div>
+        <div className="p-messContent">
+          <p>{content}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Comment
