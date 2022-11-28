9591930c8f2ed24141d3bdadc624ea9f6e27d8e3
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-11-28 23:09:44 +0800

    add ellipsis to posts in homepage

--- src/01-cart/pages/CartList.jsx
+++ src/01-cart/pages/CartList.jsx
@@ -2,7 +2,7 @@ import { useContext, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 // scss
-import './../styles/CartInfo.scss'
+import './../styles/CartList.scss'
 
 // components
 import NavBar from '../../00-homepage/components/NavBar'
@@ -24,17 +24,15 @@ import ProgressIcon from './../../dotown/warrior.png'
 import PickupIcon from './../../dotown/hamburger.png'
 import ShopCover from './../images/01cover.jpg'
 
+// cart init
+// initialState = {
+//   items: [],
+//   isEmpty: true,
+//   totalItems: 0,
+//   cartTotal: 0,
+// }
 
-
-function CartInfo(props) {
-  const [quantity, setQuantity] = useState([])
-
-  // 商品金額小計
-  const [totalPrice, setTotalPrice] = useState([])
-
-  // 修改過數量的商品金額小計
-  const [newTotalPrice, setNewTotalPrice] = useState(0)
-
+function CartList(props) {
   return (
     <>
       <div className="y-CartList-container">
@@ -69,19 +67,55 @@ function CartInfo(props) {
           </div>
         </div>
         <div className="y-Cart-middle">
-          <div className="y-Cart-pickup  y-Cart-sections">
-            <p className="y-Cart-tab y-Cart-pickup-tab">取餐方式</p>
-            <div className="y-Cart-main y-Cart-pickup-main"></div>
-          </div>
-          <div className="y-Cart-rec  y-Cart-sections">
-            <p className="y-Cart-tab y-Cart-rec-tab">推薦加購</p>
-            <div className="y-Cart-rec-top"></div>
-            <div className="y-Cart-rec-bottom">
-              <div className="y-Cart-rec-row"></div>
+          <div className="y-Cart-shop">
+            <div className="y-Cart-shop-border">
+              <div className="y-Cart-shop-cover">
+                <img src={ShopCover} alt="shop cover" />
+              </div>
+              <div className="y-Cart-shop-info">
+                <div className="y-Cart-shop-top">
+                  <p className="y-Cart-shop-name">惜食店家 Shop</p>
+                  <div className="y-Cart-shop-status">
+                    <OpenHoursBtn />
+                  </div>
+                </div>
+                <ul className="y-Cart-shop-bottom">
+                  <li className="y-Cart-shop-tel">02-12345678</li>
+                  <li className="y-Cart-shop-address">
+                    台北市大安區復興南路一段390號
+                  </li>
+                  <li className="y-Cart-shop-time">營業時間： 10:00-21:00</li>
+                </ul>
+              </div>
+              <div className="y-Cart-shop-pickup">
+                <div className="y-Cart-pickup-top">
+                  <p className="y-Cart-pickup-title">取餐資訊</p>
+                  <div className="y-Cart-pickup-status">
+                    <PickupHoursBtn />
+                  </div>
+                </div>
+                <ul className="y-Cart-pickup-bottom">
+                  <li className="y-Cart-pickup-time">取餐時間： 11:00-20:30</li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <div className="y-Cart-pickup-border">
+                  <div className="y-Cart-pickup-icon">
+                    <img src={PickupIcon} alt="hambuger icon" />
+                  </div>
+                  <div className="y-Cart-pickup-info">
+                    <p className="y-Cart-pickup-info-lg">點我看詳細取餐說明</p>
+                    <p className="y-Cart-pickup-info-sm">取餐說明</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="y-Cart-details y-Cart-sections">
-            <p className="y-Cart-tab y-Cart-details-tab">餐點明細</p>
+            <div className="y-empty-cart-wrap">
+              <EmptyCartBtn />
+            </div>
+            <p className="y-Cart-tab y-Cart-details-tab">明細一覽</p>
             <div className="y-Cart-details-top">
               <p className="y-Cart-details-name y-Cart-details-header">
                 商品名稱
@@ -97,7 +131,20 @@ function CartInfo(props) {
                 更多動作
               </p>
             </div>
-            <div className="y-Cart-details-area"></div>
+            <div className="y-Cart-details-area">
+              <div className="y-Cart-details-row">
+                <CartItemsList />
+              </div>
+              <div className="y-Cart-details-row">
+                <CartItemsList />
+              </div>
+              <div className="y-Cart-details-row">
+                <CartItemsList />
+              </div>
+              <div className="y-Cart-details-row">
+                <CartItemsList />
+              </div>
+            </div>
             <div className="y-Cart-details-bottom">
               <p className="y-Cart-details-total">
                 共 1 項商品，數量 1 個，總金額NT$ 537 元
@@ -108,6 +155,30 @@ function CartInfo(props) {
                 </div>
                 <div className="y-cart-pay-wrap">
                   <GoPayBtn />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="y-Cart-rec  y-Cart-sections">
+            <p className="y-Cart-tab y-Cart-rec-tab">推薦加購</p>
+            <div className="y-Cart-rec-top">
+              <p className="y-Cart-rec-header">
+                以下是來自「好ㄘ早午餐」的更多寶物，錯過會很可惜的...
+              </p>
+            </div>
+            <div className="y-Cart-rec-bottom">
+              <div className="y-Cart-rec-row">
+                <div className="y-Cart-rec-wrap">
+                  <RecMerch />
+                </div>
+                <div className="y-Cart-rec-wrap">
+                  <RecMerch />
+                </div>
+                <div className="y-Cart-rec-wrap">
+                  <RecMerch />
+                </div>
+                <div className="y-Cart-rec-wrap">
+                  <RecMerch />
                 </div>
               </div>
             </div>
@@ -126,4 +197,4 @@ function CartInfo(props) {
   )
 }
 
-export default CartInfo
+export default CartList
