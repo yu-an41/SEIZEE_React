77b6beb90549c6804c3a429d0c6dafccefcf6363
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-12-01 16:04:39 +0800

    update 03-shop

--- src/03-shop/shop-config.js
+++ src/03-shop/shop-config.js
@@ -1,7 +1,4 @@
-// export const imgUrl = 'http://localhost:3000'
+export const imgUrl = 'http://localhost:3000'
 
-<<<<<<< HEAD
 export const imgServerUrl = 'http://localhost:3004'
-=======
 // export const imgServerUrl = 'http://localhost:3002'
->>>>>>> 4d0636de6cc68618b47ffbd9474d4bfd6e477538
